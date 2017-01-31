473d375b4855e24f611ccdd6797d571f428108b7
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-02-01 02:19:33 +0530

    Removed the prints

--- upwards/eligibility/views.py
+++ upwards/eligibility/views.py
@@ -67,7 +67,6 @@ class ProfessionCreate(APIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
-            print serializer.errors
             return Response({}, status=status.HTTP_400_BAD_REQUEST)
         return Response({}, status.HTTP_401_UNAUTHORIZED)
 
