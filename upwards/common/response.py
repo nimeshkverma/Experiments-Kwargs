f6087c154d6a9931040fa615c689dda7d35d8878
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-02-03 02:08:17 +0530

    Removed the response Bug

--- upwards/common/response.py
+++ upwards/common/response.py
@@ -13,4 +13,4 @@ class MetaDataResponse(Response):
             if len(args) >= 2:
                 MetaDataResponse.meta_data_dict["meta"] = args[1]
             modified_args = tuple([MetaDataResponse.meta_data_dict])
-        super(MetaDataResponse, self).__init__(modified_args, **kwargs)
+        super(MetaDataResponse, self).__init__(*modified_args, **kwargs)
