a3f1ad5d2581749ab4c6ce042698b16f2b2099a4
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-02-14 22:25:58 +0530

    Borrower work mid

--- upwards/participant/models.py
+++ upwards/participant/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.db.models.signals import post_save
 
-from common.models import InActiveModel, ActiveObjectManager
+from common.models import InActiveModel, ActiveObjectManager, ActiveModel
 from activity.models import register_customer_state
 from activity.model_constants import ELIGIBILITY_RESULT_APPROVED_STATE
 
@@ -11,6 +11,8 @@ from activity.model_constants import ELIGIBILITY_RESULT_APPROVED_STATE
 class Borrower(InActiveModel):
     customer = models.OneToOneField(
         'customer.Customer', on_delete=models.CASCADE)
+    borrower_type = models.ForeignKey(
+        'BorrowerType', on_delete=models.CASCADE, blank=True, null=True)
     credit_limit = models.IntegerField()
     number_of_active_loans = models.IntegerField(default=0)
     number_of_repaid_loans = models.IntegerField(default=0)
@@ -32,3 +34,24 @@ class Borrower(InActiveModel):
 
 post_save.connect(
     Borrower.register_eligibility_result_approved_customer_state, sender=Borrower)
+
+
+class BorrowerType(ActiveModel):
+    type_name = models.CharField(
+        max_length=100, unique=True)
+    max_current_loans_allowed = models.IntegerField(default=1)
+
+
+class Lender(ActiveModel):
+    name = models.CharField(max_length=100, unique=True)
+
+
+# lender_id
+# name
+# type
+# allocation_limit
+# created_at
+# updated_at
+# is_active
+# borrower_type
+# borrower_type_id
