7a1fdf910c965edb38329f36ca84303d0456300e
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-03-06 22:31:43 +0530

    Taking Esign settings from settings

--- upwards/aadhaar/services/esign_constants.py
+++ upwards/aadhaar/services/esign_constants.py
@@ -9,7 +9,7 @@ SIGNED_PDF_PATH = "pdfs/customer_{customer_id}/customer_{customer_id}_signed_loa
 PDF_DIRECTORY = "pdfs/customer_{customer_id}"
 SIGN_DOCUMENT_COMMANDS = {
     "new_directory": "mkdir " + PDF_DIRECTORY,
-    "make_unsigned_pdf": "wkhtmltopdf --zoom 4 " + PDF_URL + " " + UNSIGNED_PDF_PATH,
+    "make_unsigned_pdf": "wkhtmltopdf --zoom " + str(settings.PDF_CONVERSION['zoom']) + " " + PDF_URL + " " + UNSIGNED_PDF_PATH,
     "delete_directory": "rm -rf " + PDF_DIRECTORY,
 }
 
